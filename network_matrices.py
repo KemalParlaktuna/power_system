898cc60fe3076c62da40b77c0b444c784d7f6a85
KemalParlaktuna <kparlaktuna@gmail.com>
2025-06-17 11:32:00 +0300

    Update network_matrices.py
    Add index mapping for networks that are not sorted

--- network_matrices.py
+++ network_matrices.py
@@ -8,12 +8,12 @@ def create_y_bus(net):
     net.y_bus_to_from = lil_matrix((len(net.lines) + len(net.transformers), len(net.buses)), dtype=cdouble)
 
     for shunt in net.shunts.values():
-        i = shunt.bus.bus_idx
+        i = net.bus_map[shunt.bus.bus_idx]
         net.full_y_bus[i, i] += (shunt.p_mw - 1j*shunt.q_mvar)/net.s_base_mva
 
     for line in net.lines.values():
-        i = line.from_bus.bus_idx
-        j = line.to_bus.bus_idx
+        i = net.bus_map[line.from_bus.bus_idx]
+        j = net.bus_map[line.to_bus.bus_idx]
         r_pu = net.ohm_to_pu(line.r_ohm, line.from_bus.voltage_level_kv)
         x_pu = net.ohm_to_pu(line.x_ohm, line.from_bus.voltage_level_kv)
         b_total_pu = net.mho_to_pu(line.b_total_mho, line.from_bus.voltage_level_kv)
@@ -22,18 +22,18 @@ def create_y_bus(net):
         net.full_y_bus[i, j] -= y_series
         net.full_y_bus[j, i] -= y_series
         net.full_y_bus[j, j] += y_series + 1j * b_total_pu/2
+        line_idx = net.line_map[line.idx]
+        net.y_bus_from_to[line_idx, i] -= -y_series - 1j * b_total_pu/2
+        net.y_bus_from_to[line_idx, j] += -y_series
 
-        net.y_bus_from_to[line.idx, i] -= -y_series - 1j * b_total_pu/2
-        net.y_bus_from_to[line.idx, j] += -y_series
-
-        net.y_bus_to_from[line.idx, i] += -y_series
-        net.y_bus_to_from[line.idx, j] -= -y_series - 1j * b_total_pu / 2
+        net.y_bus_to_from[line_idx, i] += -y_series
+        net.y_bus_to_from[line_idx, j] -= -y_series - 1j * b_total_pu / 2
         net.from_buses.append(i)
         net.to_buses.append(j)
 
     for transformer in net.transformers.values():
-        i = transformer.from_bus.bus_idx
-        j = transformer.to_bus.bus_idx
+        i = net.bus_map[transformer.from_bus.bus_idx]
+        j = net.bus_map[transformer.to_bus.bus_idx]
         z_base_transformer = transformer.v_rated_low_kv**2/transformer.rated_s_mva
         r_pu = net.ohm_to_pu(transformer.r_pu*z_base_transformer, transformer.v_rated_low_kv)
         x_pu = net.ohm_to_pu(transformer.x_pu*z_base_transformer, transformer.v_rated_low_kv)
@@ -53,12 +53,12 @@ def create_y_bus(net):
         net.full_y_bus[i, j] -= y_series/conjugate(a)
         net.full_y_bus[j, i] -= y_series/a
         net.full_y_bus[j, j] += y_series + y_shunt/2
+        transformer_idx = net.transformer_map[transformer.idx]
+        net.y_bus_from_to[transformer_idx, i] -= -y_series - y_shunt/2
+        net.y_bus_from_to[transformer_idx, j] += -y_series
 
-        net.y_bus_from_to[transformer.idx, i] -= -y_series - y_shunt/2
-        net.y_bus_from_to[transformer.idx, j] += -y_series
-
-        net.y_bus_to_from[transformer.idx, i] += -y_series
-        net.y_bus_to_from[transformer.idx, j] -= -y_series - y_shunt/2
+        net.y_bus_to_from[transformer_idx, i] += -y_series
+        net.y_bus_to_from[transformer_idx, j] -= -y_series - y_shunt/2
 
         net.from_buses.append(i)
         net.to_buses.append(j)
